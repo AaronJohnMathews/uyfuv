89f044158d84ef9b360233422c117d6ba2ca5ecc
AaronJohnMathews <123355753+AaronJohnMathews@users.noreply.github.com>
2023-07-18 12:11:08 +0530

    Add files via upload

---
+++ crt.java
@@ -0,0 +1,12 @@
+public class crt {
+
+    public static void main(String args[]) {
+        int a = 5;
+        int c = 6;
+        float b = 3.2f;
+        float sum = a + b;
+        System.out.println("Hi everyone!");
+        System.out.println("Sum:" + sum);
+
+    }
+}
